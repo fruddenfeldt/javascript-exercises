406473dc1ed4a60d13f92334953c069691a162e2
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-23 21:48:58 +0200

    first iteration that fails all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+// Pseudocode:
+// Create a for loop 
+// With "if" statement to check for equality, like 3+5 == 8
 
-};
 
+const fibonacci = function fibo(num){
+    var a = 1, b = 0, temp;
+  
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  }
+  
+  // fibonacci(6) // returns 13
+  
 // Do not edit below this line
 module.exports = fibonacci;
